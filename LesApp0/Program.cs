45fbf45ea33ecf5f6a68c9a6d7eb379950206189
Bogdan <pinchuk.brus@gmail.com>
2019-08-06 14:12:06 +0300

    Add LesApp0

---
+++ LesApp0/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LesApp0
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Join Unicode
+            Console.OutputEncoding = Encoding.Unicode;
+
+
+            // delay
+            Console.ReadKey(true);
+        }
+
+
+    }
+}
